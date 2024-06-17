a5539f930b4f8c4d420e0ab20ec9f839798f2d24
Ellis <ellissiadis@gmail.com>
2024-06-18 00:53:42 +0200

    Added dark mode to AppAppearance.js

--- pages/AppAppearance.js
+++ pages/AppAppearance.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import * as React from 'react';
+import { useState, useContext } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Switch } from 'react-native';
+import { EventRegister } from 'react-native-event-listeners';
+import themeContext from '../theme/themeContext';
 
 const AppAppearance = () => {
+
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = useContext(themeContext);
+
   return (
-    <View style={styles.container}>
-      <Text>AppAppearance Screen</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+    <View style={styles.settingRow}>
+      <Text style={[styles.settingLabel, { color: theme.color }]}>Dark Theme</Text>
+      <Switch
+        value={darkMode}
+        onValueChange={(value) => {
+          setDarkMode(value);
+          EventRegister.emit('ChangeTheme', value);
+        }}
+      />
     </View>
+  </SafeAreaView>
   );
 };
 
